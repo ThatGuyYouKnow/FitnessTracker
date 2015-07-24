9435d49ad09cbf74767a90f7feef9e06dd32c678
Andy <andrew.fleer@gmail.com>
2015-07-24 17:53:48 -0500

    Resolving static files: added a way to host and serve up pdf files

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -13,14 +13,14 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
 		
 		System.out.println("exercise: " + exercise.getMinutes());
-		//return "forward:addMoreMinutes.html";
-		return "redirect:addMoreMinutes.html";
+
+		return "MoreMinutes.html";
 	}
 	
-	@RequestMapping(value="/addMoreMinutes")
-	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
-		
-		System.out.println("exercising: " + exercise.getMinutes());
-		return "addMinutes";
-	}
+//	@RequestMapping(value="/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//		
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		return "addMinutes";
+//	}
 }
