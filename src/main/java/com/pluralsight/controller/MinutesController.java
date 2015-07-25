94957a1391d4b0d34fa62f7dc5c516558d41a1d5
Andy <andrew.fleer@gmail.com>
2015-07-24 21:23:32 -0500

    Cleaned up a little bit. Added a service tier

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -1,8 +1,8 @@
 package com.pluralsight.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
+import com.pluralsight.service.ExerciseService;
 
 @Controller
 public class MinutesController {
+	
+	@Autowired
+	private ExerciseService exerciseService;
 
 	@RequestMapping(value="/addMinutes")
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
@@ -26,21 +30,7 @@ public class MinutesController {
 	
 	@RequestMapping(value = "/activities", method = RequestMethod.GET)
 	public @ResponseBody List<Activity> findAllActivites() {
-		List<Activity> activities = new ArrayList<Activity>();
-		
-		Activity run = new Activity();
-		run.setDesc("Run");
-		activities.add(run);
-		
-		Activity bike = new Activity();
-		bike.setDesc("Bike");
-		activities.add(bike);
-
-		Activity swim = new Activity();
-		swim.setDesc("Swim");
-		activities.add(swim);
-		
-		return activities;
+		return exerciseService.findAllActivites();
 	}
 	
 //	@RequestMapping(value="/addMoreMinutes")
